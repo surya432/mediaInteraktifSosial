4f34be80735cf108eea7a8f500d2dbf2888ee6ce
unknown <hadisurya295@gmail.com>
2020-11-21 19:01:02 +0700

    modif cuk

--- lib/app/ui/Home/HomeScreen.dart
+++ lib/app/ui/Home/HomeScreen.dart
@@ -19,87 +19,112 @@ class _HomeScreenState extends State<HomeScreen> {
     super.initState();
   }
 
+  Future<bool> _onWillPop() async {
+    return (await showDialog(
+          context: context,
+          builder: (context) => new AlertDialog(
+            title: new Text('Are you sure?'),
+            content: new Text('Do you want to exit an App'),
+            actions: <Widget>[
+              new FlatButton(
+                onPressed: () => Navigator.of(context).pop(false),
+                child: new Text('No'),
+              ),
+              new FlatButton(
+                onPressed: () => Navigator.of(context).pop(true),
+                child: new Text('Yes'),
+              ),
+            ],
+          ),
+        )) ??
+        false;
+  }
+
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
-      body: BGContainerWidget(
-        defaultImages: "assets/images/BG02-01.jpg",
-        custombar: Row(
-          crossAxisAlignment: CrossAxisAlignment.start,
-          mainAxisAlignment: MainAxisAlignment.spaceBetween,
-          children: [
-            GestureDetector(
-              child: Image.asset(
-                "assets/button/btn-03.png",
-                height: Get.height * .1,
-                width: Get.height * .1,
-              ),
-            ),
-            GestureDetector(
-              child: Image.asset(
-                "assets/button/btn-05.png",
-                height: Get.height * .1,
-                width: Get.height * .1,
-              ),
-            ),
-          ],
-        ),
-        kPaddingTop: context.mediaQueryPadding.top,
-        content: Container(
-          // color: Colors.grey[700],
-          height: context.isTablet ? Get.height * .8 : Get.height * .75,
-          width: context.isTablet ? Get.width * .78 : Get.width * .75,
-          child: Stack(
-            fit: StackFit.loose,
+    return WillPopScope(
+      onWillPop: _onWillPop,
+      child: Scaffold(
+        body: BGContainerWidget(
+          defaultImages: "assets/images/BG02-01.jpg",
+          custombar: Row(
+            crossAxisAlignment: CrossAxisAlignment.start,
+            mainAxisAlignment: MainAxisAlignment.spaceBetween,
             children: [
-              Positioned(
-                top: context.isTablet ? Get.height * .05 : 0,
-                left: 0,
-                right: 0,
+              GestureDetector(
                 child: Image.asset(
-                  "assets/images/title.png",
-                  height: Get.height * .17,
+                  "assets/button/btn-03.png",
+                  height: Get.height * .1,
+                  width: Get.height * .1,
                 ),
               ),
-              Positioned(
-                bottom: 0,
-                left: 0,
-                right: 0,
-                child: Row(
-                  crossAxisAlignment: CrossAxisAlignment.end,
-                  mainAxisAlignment: MainAxisAlignment.center,
-                  children: [
-                    GestureDetector(
-                      onTap: () =>
-                          Get.to(SettingScreen(), transition: Transition.zoom),
-                      child: Image.asset(
-                        "assets/button/btn-08.png",
-                        height: Get.height * .1,
-                        width: Get.height * .1,
-                      ),
-                    ),
-                    SizedBox(width: 8),
-                    GestureDetector(
-                      onTap: () => Get.toNamed("/MATERI"),
-                      child: Image.asset(
-                        "assets/button/btn-01.png",
-                        height: Get.height * .15,
-                        width: Get.height * .15,
-                      ),
-                    ),
-                    SizedBox(width: 8),
-                    GestureDetector(
-                      onTap: () => Get.to(QuizScreen()),
-                      child: Image.asset(
-                        "assets/button/btn-12.png",
-                        height: Get.height * .1,
-                        width: Get.height * .1,
-                      ),
-                    )
-                  ],
+              GestureDetector(
+                onTap: _onWillPop,
+                child: Image.asset(
+                  "assets/button/btn-05.png",
+                  height: Get.height * .1,
+                  width: Get.height * .1,
                 ),
-              )
+              ),
             ],
+          ),
+          kPaddingTop: context.mediaQueryPadding.top,
+          content: Container(
+            // color: Colors.grey[700],
+            height: context.isTablet ? Get.height * .8 : Get.height * .75,
+            width: context.isTablet ? Get.width * .78 : Get.width * .75,
+            child: Stack(
+              fit: StackFit.loose,
+              children: [
+                Positioned(
+                  top: context.isTablet ? Get.height * .05 : 0,
+                  left: 0,
+                  right: 0,
+                  child: Image.asset(
+                    "assets/images/title.png",
+                    height: Get.height * .17,
+                  ),
+                ),
+                Positioned(
+                  bottom: 0,
+                  left: 0,
+                  right: 0,
+                  child: Row(
+                    crossAxisAlignment: CrossAxisAlignment.end,
+                    mainAxisAlignment: MainAxisAlignment.center,
+                    children: [
+                      GestureDetector(
+                        onTap: () => Get.to(SettingScreen(),
+                            transition: Transition.zoom),
+                        child: Image.asset(
+                          "assets/button/btn-08.png",
+                          height: Get.height * .1,
+                          width: Get.height * .1,
+                        ),
+                      ),
+                      SizedBox(width: 8),
+                      GestureDetector(
+                        onTap: () => Get.toNamed("/MATERI"),
+                        child: Image.asset(
+                          "assets/button/btn-01.png",
+                          height: Get.height * .15,
+                          width: Get.height * .15,
+                        ),
+                      ),
+                      SizedBox(width: 8),
+                      GestureDetector(
+                        onTap: () => Get.to(QuizScreen()),
+                        child: Image.asset(
+                          "assets/button/btn-12.png",
+                          height: Get.height * .1,
+                          width: Get.height * .1,
+                        ),
+                      )
+                    ],
+                  ),
+                )
+              ],
+            ),
           ),
         ),
       ),
